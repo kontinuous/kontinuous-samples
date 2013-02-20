09bf31496988a6783ecaeb182f73c66fb70f0a44
khamutov <khamutov@ailabs.ru>
2013-02-20 11:15:24 +0400

    fixes total

--- public/js/kanban.js
+++ public/js/kanban.js
@@ -25,49 +25,12 @@ function TaskCtrl($scope, Task) {
     {id: 3, name: "done"}
   ];
 
-  /*[
-    {
-      name: 'todo',
-      tasks: [
-      ]
-    },
-    {
-      name: 'progress',
-      tasks: [
-      ]
-    },
-    {
-      name: 'done',
-      tasks: [
-      ]
-    }
-  ];*/
-  $scope.task_filter = function(filter) {
-    var filtered = [];
-    angular.forEach($scope.tasks, function(task) {
-      if(task.status === filter) {
-        filtered.push(task);
-      }
-    });
-    return filtered;
-  };
-
   $scope.addTodo = function() {
     var newTask = new Task({name:$scope.todoText});
     newTask.$save(function() {
       $scope.tasks = $scope.updateModel();
     });
     $scope.todoText = '';
-  };
-
-  $scope.total = function(filter) {
-    var total = 0;
-    angular.forEach($scope.tasks, function(task_group) {
-      if(task_group.name === filter) {
-        total = task_group.tasks.length;
-      }
-    });
-    return total;
   };
 
   $scope.syncModel = function (e, ui) {
